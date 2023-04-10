5d46888e20501f1d3db0eef8444d0e4ceeae87ff
Anderson <anderson.fagundes4@gmail.com>
2023-04-10 12:02:33 +0100

    useForm

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,33 +2,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
 
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const username = useForm();
+    const password = useForm();
 
     function handleSubmit(event){
+        
         event.preventDefault();
-        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({username, password}),    
-        }).then(response => {
-            return response.json();    
-        }).then((json) => {
-            console.log(json);
-        });
+
+        if(username.validate() && password.validate()){
+
+            fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(),    
+            }).then(response => {
+                return response.json();    
+            }).then((json) => {
+                console.log(json);
+            });
+        
+        }
+    
     }
 
     return (
         <section>
             <h1>Login</h1>
             <form action="" onSubmit={handleSubmit}>
-                <Input label="User" type="text" name="username" />
-                <Input label="Password" type="password" name="password" />
+                <Input label="User" type="text" name="username" {...username} />
+                <Input label="Password" type="password" name="password" {...password} />
                 <Button>Entrar</Button>
                 {/* <input type="text" value={username} onChange={({target}) => setUsername(target.value)} />
                 <input type="password" value={password} onChange={({target}) => setPassword(target.value)} />
