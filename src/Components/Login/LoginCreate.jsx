408246810b77f5a3b16a40b7852ea1d71ca02574
Anderson <anderson.fagundes4@gmail.com>
2023-08-06 16:31:26 +0100

    useFetch

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import Error from '../Helper/Error';
 import useForm from '../../Hooks/useForm';
 import { USER_POST } from '../../Api';
 import { UserContext } from '../../UserContext';
+import useFetch from '../../Hooks/useFetch';
 
 const LoginCreate = () => {
   const username = useForm();
@@ -11,6 +13,7 @@ const LoginCreate = () => {
   const password = useForm('password');
 
   const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -21,7 +24,7 @@ const LoginCreate = () => {
       password: password.value,
     });
 
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) {
       userLogin(username.value, password.value);
     }
@@ -30,12 +33,17 @@ const LoginCreate = () => {
 
   return (
     <section className="animationLeft">
-      <h1 className="title">Register</h1>
+      <h1 className="title">Sign up</h1>
       <form onSubmit={handleSubmit}>
         <Input label="User" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Password" type="password" name="password" {...password} />
-        <Button>Register</Button>
+        {loading ? (
+          <Button disabled>Signing up...</Button>
+        ) : (
+          <Button>Sign up</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
